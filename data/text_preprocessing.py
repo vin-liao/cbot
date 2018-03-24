f840772357849e957c636ae56f6c3ed7a44a4b80
Vincent Liao <vincent.liao@protonmail.com>
2018-03-24 17:57:59 +0700

    change data source

--- data/text_preprocessing.py
+++ data/text_preprocessing.py
@@ -1,27 +1,11 @@
 import re
 
-with open('./linux.txt', 'r+') as f:
+with open('./linux_raw.txt', 'r+') as f:
 	text = f.read()
 
 	#remove comment
 	text = re.sub('/\*.*?\*/', '', text, flags=re.DOTALL)
 	text = re.sub('//.*?\n', '', text)
-
-	'''
-	replacing whitespace, so that the NN can pick this up,
-	later on the NN can generate it's own whitespace, so
-	humans can read their code.
-
-	Later, after the generated text is done, these newlines
-	that the NN generated is reversed, so it's readable.
-
-	i.e.
-	`n` -> \n
-	'''
-	text = re.sub('\n', '`n`', text)
-	text = re.sub('\r', '`r`', text)
-	text = re.sub('\f', '`f`', text)
-	text = re.sub('\t', '`t`', text)
 
 	f.seek(0)
 	f.write(text)
