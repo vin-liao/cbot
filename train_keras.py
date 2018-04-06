44c5eb4a6e88dfbe02549ba780aa96d34f862d2b
Vincent Liao <vincent.liao@protonmail.com>
2018-04-06 18:15:07 +0700

    Add class

--- train_keras.py
+++ train_keras.py
@@ -1,83 +1,92 @@
 import numpy as np
-import data_utils
-from keras.models import Sequential
+from utils import Data
 import keras.optimizers
 from keras.callbacks import LambdaCallback
-from keras.layers import Dense, Embedding, GlobalMaxPooling1D, CuDNNLSTM, Dropout, BatchNormalization, Activation, LSTM
-from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GlobalMaxPooling1D, CuDNNLSTM, Dropout, BatchNormalization, Activation
 import getopt
 import sys
-
-seq_len = 50
-num_epoch = 25
-batch_size = 32
-num_char = 500
-num_gen = 20
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 's:c:e:b:g:', ['seq=', 'char=', 'epoch=', 'batch=', 'gen='])
-except getopt.GetoptError:
-    sys.exit(2)
-
-for opt, arg in opts:
-	if opt in ('-s', '--seq'):
-		seq_len = int(arg)
-	elif opt in ('-c', '--char'):
-		num_char = int(arg)
-	elif opt in ('-e', '--epoch'):
-		num_epoch = int(arg)
-	elif opt in ('-b', '--batch'):
-		batch_size = int(arg)
-	elif opt in ('-g', '--gen'):
-		num_gen = int(arg)
-	else:
-		sys.exit(2)
+from sklearn.utils import shuffle
 
 #prediction
-def predict():
-	eval_matrix = data_utils.generate_sample(seq_len, num_char)
-	yhat_matrix = np.zeros((1, eval_matrix.shape[2]))
-	text = ''
-	for _ in range(num_gen):
-		yhat_matrix_raw = model.predict(eval_matrix)
-		yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 1
+def predict(use_empty_vector=False):
+	if use_empty_vector:
+		#returns a zero vectors
+		eval_matrix = data.generate_sample(zero_vectors=True)
+		yhat_matrix = np.zeros((1, num_classes))
+		text = ''
+		for i in range(num_gen):
+			if i == 0:
+				yhat_matrix[0, np.random.choice(eval_matrix.shape[2])] = 1	
 
-		yhat_char = data_utils.indices_to_char(yhat_matrix)
-		text += yhat_char
+			else:
+				yhat_matrix_raw = model.predict(eval_matrix)
+				yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 1
 
-		eval_matrix = np.delete(eval_matrix, 0, axis=1)
-		eval_matrix = np.append(eval_matrix, [yhat_matrix], axis=1)
+				yhat_char = data.indices_to_char(yhat_matrix)
+				text += yhat_char
 
-		yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 0
+				eval_matrix = np.delete(eval_matrix, 0, axis=1)
+				eval_matrix = np.append(eval_matrix, [yhat_matrix], axis=1)
 
-	print(text)
+				yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 0
 
-x, y = data_utils.generate_data(seq_len, num_char)
-num_classes = x.shape[2]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05, random_state=42)
-generate_callback = LambdaCallback(on_epoch_end=lambda batch, logs: predict())
+		print(text)
 
-model = Sequential()
-model.add(CuDNNLSTM(128, input_shape=(seq_len, num_classes), return_sequences=True))
-model.add(Activation('relu'))
-model.add(BatchNormalization())
-model.add(Dropout(.7))
+def main():
+	seq_len = 50
+	num_epoch = 25
+	batch_size = 32
+	num_char = 100
+	num_gen = 20
 
-model.add(CuDNNLSTM(64))
-model.add(Activation('relu'))
-model.add(BatchNormalization())
-model.add(Dropout(.5))
+	try:
+	    opts, args = getopt.getopt(sys.argv[1:], 's:c:e:b:g:', ['seq=', 'char=', 'epoch=', 'batch=', 'gen='])
+	except getopt.GetoptError:
+	    sys.exit(2)
 
-model.add(Dense(num_classes, activation='softmax'))
+	for opt, arg in opts:
+		if opt in ('-s', '--seq'):
+			seq_len = int(arg)
+		elif opt in ('-c', '--char'):
+			num_char = int(arg)
+		elif opt in ('-e', '--epoch'):
+			num_epoch = int(arg)
+		elif opt in ('-b', '--batch'):
+			batch_size = int(arg)
+		elif opt in ('-g', '--gen'):
+			num_gen = int(arg)
+		else:
+			sys.exit(2)
 
-adam = keras.optimizers.Adam(lr=0.001, clipvalue=1000)
-model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
-print(model.summary())
+	data = Data.Data(seq_len, hm_char=num_char)
+	x, y = data.generate_data()
+	num_classes = x.shape[2]
+	x, y = shuffle(x, y, random_state=42)
+	generate_callback = LambdaCallback(on_epoch_end=lambda batch, logs: predict(use_empty_vector=True))
 
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=num_epoch, batch_size=batch_size, verbose=2, callbacks=[generate_callback])
-scores = model.evaluate(x_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+	model = Sequential()
+	model.add(CuDNNLSTM(200, input_shape=(seq_len, num_classes), return_sequences=True))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(.5))
 
-print('Saving model...')
-model.save('../char_rnn_model.h5')
-print('Finished saving!')
+	model.add(CuDNNLSTM(200))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Dropout(.5))
+
+	model.add(Dense(num_classes, activation='softmax'))
+
+	adam = keras.optimizers.Adam(lr=0.001, clipvalue=1000)
+	model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+	print(model.summary())
+
+	model.fit(x, y, epochs=num_epoch, batch_size=batch_size, verbose=1, callbacks=[generate_callback])
+
+	print('Saving model...')
+	model.save_weights('../char_rnn_model_weights.h5')
+	print('Finished saving!')
+
+if __name__ == '__main__':
+	main()
---
+++ utils/Data.py
@@ -0,0 +1,100 @@
+import numpy as np
+import re
+
+class Data():
+	def __init__(self, seq_len, hm_char=10000, skip_char=1):
+		'''
+		seq_len: Sequence length.
+		
+		hm_char: How many char is taken from the text. the default is 10k, so only
+				 the first 10k characters is used
+
+		return_generator: If True, returns a generator
+						  If false, returns (train, target)
+
+		skip_char: How many characters to skip in the training data. Defualt is 1, 
+				   which means it doesn't skip.
+
+				   e.g.
+				   skip 1
+				   The quick brown fox ju -> m
+				   he quick brown fox jum -> p
+				   e quick brown fox jump -> s
+
+				   skip 3
+				   The quick brown fox ju -> m
+				    quick brown fox jumps -> 
+				   ck brown fox jumps ove -> r
+
+		'''
+		self.sentences = []
+		self.next_char = []
+		self.text = ''
+		self.seq_len = seq_len
+		self.hm_char = hm_char
+		self.skip_char = skip_char
+
+	def generate_data(self, save_sentence=False):
+		with open('./data/linux.txt', 'r') as f:
+			for line in f:
+				self.text += line.lower()
+			f.close
+
+		#hm_char -1 uses all data
+		if self.hm_char == -1:
+			pass
+		else:
+			self.text = self.text[:self.hm_char]
+
+		self.chars = sorted(list(set(self.text)))
+		self.char_len = len(self.chars)
+		self.train_size = self.char_len-self.seq_len
+
+		self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
+		self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
+
+		#create sentences and their next character
+		for i in range(0, len(self.text)-self.seq_len, self.skip_char):
+			self.sentences.append(self.text[i:i+self.seq_len])
+			self.next_char.append(self.text[i+self.seq_len])
+
+		#map each character to one hot
+		self.x = np.zeros((len(self.sentences), self.seq_len, len(self.chars)), dtype=np.float32)
+		self.y = np.zeros((len(self.sentences), len(self.chars)), dtype=np.float32)
+		for i, sentence in enumerate(self.sentences):
+			for j, char in enumerate(sentence):
+				x[i, j, self.char_indices[char]] = 1
+			y[i, self.char_indices[next_char[i]]] = 1
+
+		if save_sentence:
+			pass
+
+		return self.x, self.y
+
+	def generate_sample(self, zero_vectors=False):
+		if zero_vectors:
+			if len(self.sentences) == 0:
+				generate_data(self.seq_len, self.hm_char)
+			return np.zeros((1, self.seq_len, self.char_len), dtype=np.float32)
+
+		else:
+			if len(self.sentences) == 0:
+				generate_data(self.seq_len, self.hm_char)
+
+			sample_matrix = np.zeros((1, self.seq_len, self.char_len), dtype=np.float32)
+			seed = np.random.randint(len(self.sentences))	
+			one_sentence = self.sentences[seed]
+
+			for i, char in enumerate(one_sentence):
+				sample_matrix[0, i, self.char_indices[char]] = 1
+
+			return sample_matrix
+
+	def char_to_indices(self, char):
+		mat = np.zeros(self.char_len, dtype=np.float32)
+		mat[self.char_indices[char]] = 1
+		return mat
+
+	def indices_to_char(self, mat):
+		idx = np.where(mat[0]==1)
+		return self.indices_char[idx[0][0]]
