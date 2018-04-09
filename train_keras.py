7ff3a827d84fe02b695c52c56a5c7906b37a3e4b
Vincent Liao <vincent.liao@protonmail.com>
2018-04-09 08:51:53 +0700

    added generator

--- train_keras.py
+++ train_keras.py
@@ -3,90 +3,74 @@ from utils import Data
 import keras.optimizers
 from keras.callbacks import LambdaCallback
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, GlobalMaxPooling1D, CuDNNLSTM, Dropout, BatchNormalization, Activation
+from keras.layers import Dense, Embedding, GlobalMaxPooling1D, CuDNNLSTM, Dropout, BatchNormalization, Activation, LSTM
 import getopt
 import sys
 from sklearn.utils import shuffle
 
-#prediction
-def predict(use_empty_vector=False):
-	if use_empty_vector:
-		#returns a zero vectors
-		eval_matrix = data.generate_sample(zero_vectors=True)
-		yhat_matrix = np.zeros((1, num_classes))
-		text = ''
-		for i in range(num_gen):
-			if i == 0:
-				yhat_matrix[0, np.random.choice(eval_matrix.shape[2])] = 1	
+seq_len = 50
+num_epoch = 25
+batch_size = 32
+num_char = 1000
+num_gen = 20
+use_generator = True
 
-			else:
-				yhat_matrix_raw = model.predict(eval_matrix)
-				yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 1
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 's:c:e:b:g:u:', \
+    	['seq=', 'char=', 'epoch=', 'batch=', 'gen=', 'use_g=='])
+except getopt.GetoptError:
+    sys.exit(2)
 
-				yhat_char = data.indices_to_char(yhat_matrix)
-				text += yhat_char
+for opt, arg in opts:
+	if opt in ('-s', '--seq'):
+		seq_len = int(arg)
+	elif opt in ('-c', '--char'):
+		num_char = int(arg)
+	elif opt in ('-e', '--epoch'):
+		num_epoch = int(arg)
+	elif opt in ('-b', '--batch'):
+		batch_size = int(arg)
+	elif opt in ('-g', '--gen'):
+		num_gen = int(arg)
+	elif opt in ('-u', '--use_g'):
+		if arg == 1:
+			use_generator = True
+	else:
+		sys.exit(2)
 
-				eval_matrix = np.delete(eval_matrix, 0, axis=1)
-				eval_matrix = np.append(eval_matrix, [yhat_matrix], axis=1)
+data = Data.Data(seq_len, hm_char=num_char)
+num_classes = data.get_num_classes()
+training_size = data.get_training_size()
 
-				yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 0
-
-		print(text)
-
-def main():
-	seq_len = 50
-	num_epoch = 25
-	batch_size = 32
-	num_char = 100
-	num_gen = 20
-
-	try:
-	    opts, args = getopt.getopt(sys.argv[1:], 's:c:e:b:g:', ['seq=', 'char=', 'epoch=', 'batch=', 'gen='])
-	except getopt.GetoptError:
-	    sys.exit(2)
-
-	for opt, arg in opts:
-		if opt in ('-s', '--seq'):
-			seq_len = int(arg)
-		elif opt in ('-c', '--char'):
-			num_char = int(arg)
-		elif opt in ('-e', '--epoch'):
-			num_epoch = int(arg)
-		elif opt in ('-b', '--batch'):
-			batch_size = int(arg)
-		elif opt in ('-g', '--gen'):
-			num_gen = int(arg)
-		else:
-			sys.exit(2)
-
-	data = Data.Data(seq_len, hm_char=num_char)
+if use_generator == False:
 	x, y = data.generate_data()
-	num_classes = x.shape[2]
 	x, y = shuffle(x, y, random_state=42)
-	generate_callback = LambdaCallback(on_epoch_end=lambda batch, logs: predict(use_empty_vector=True))
 
-	model = Sequential()
-	model.add(CuDNNLSTM(200, input_shape=(seq_len, num_classes), return_sequences=True))
-	model.add(Activation('relu'))
-	model.add(BatchNormalization())
-	model.add(Dropout(.5))
+model = Sequential()
+model.add(CuDNNLSTM(200, input_shape=(seq_len, num_classes), return_sequences=True))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(.5))
 
-	model.add(CuDNNLSTM(200))
-	model.add(Activation('relu'))
-	model.add(BatchNormalization())
-	model.add(Dropout(.5))
+model.add(CuDNNLSTM(200))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dropout(.5))
 
-	model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(num_classes, activation='softmax'))
 
-	adam = keras.optimizers.Adam(lr=0.001, clipvalue=1000)
-	model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
-	print(model.summary())
+adam = keras.optimizers.Adam(lr=0.001, clipvalue=1000)
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+print(model.summary())
+generate_callback = LambdaCallback(on_epoch_end=lambda batch, logs: \
+	data.predict(model=model, num_gen=num_gen, use_empty_vector=False))
 
+if use_generator:
+	model.fit_generator(data.get_generator(), epochs=num_epoch, \
+		steps_per_epoch=training_size/batch_size, callbacks=[generate_callback], verbose=1)
+else:
 	model.fit(x, y, epochs=num_epoch, batch_size=batch_size, verbose=1, callbacks=[generate_callback])
 
-	print('Saving model...')
-	model.save_weights('../char_rnn_model_weights.h5')
-	print('Finished saving!')
-
-if __name__ == '__main__':
-	main()
+print('Saving model...')
+model.save_weights('../char_rnn_model_weights.h5')
+print('Finished saving!')
--- utils/Data.py
+++ utils/Data.py
@@ -32,40 +32,38 @@ class Data():
 		self.hm_char = hm_char
 		self.skip_char = skip_char
 
-	def generate_data(self, save_sentence=0):
 		with open('./data/linux.txt', 'r') as f:
-			self.text = f.read(self.hm_char)
+			if hm_char == -1:
+				#use all data, this is for generator
+				self.text = f.read()
+			else:
+				self.text = f.read(self.hm_char)
 			f.close()
 
 		self.chars = sorted(list(set(self.text)))
 		self.char_len = len(self.chars)
-		self.train_size = self.char_len-self.seq_len
 
 		self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
 		self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
+		self.num_classes = len(self.char_indices)
 
 		#create sentences and their next character
 		for i in range(0, len(self.text)-self.seq_len, self.skip_char):
 			self.sentences.append(self.text[i:i+self.seq_len])
 			self.next_char.append(self.text[i+self.seq_len])
 
-		#map each character to one hot
-		self.x = np.zeros((len(self.sentences), self.seq_len, len(self.chars)), dtype=np.float32)
-		self.y = np.zeros((len(self.sentences), len(self.chars)), dtype=np.float32)
-		for i, sentence in enumerate(self.sentences):
-			for j, char in enumerate(sentence):
-				self.x[i, j, self.char_indices[char]] = 1
-			self.y[i, self.char_indices[self.next_char[i]]] = 1
+		self.training_size = len(self.sentences)
+
+	def generate_data(self, save_sentence=0):
+		x, y = self.sentences_to_matrix(len(self.sentences), self.sentences, self.next_char)
 
 		if save_sentence > 0:
 			self.save_things(save_sentence)
 
-		return self.x, self.y
+		return x, y
 
 	def generate_sample(self, zero_vectors=False):
 		if zero_vectors:
-			if len(self.sentences) == 0:
-				generate_data(self.seq_len, self.hm_char)
 			return np.zeros((1, self.seq_len, self.char_len), dtype=np.float32)
 
 		else:
@@ -75,7 +73,7 @@ class Data():
 			sample_matrix = np.zeros((1, self.seq_len, self.char_len), dtype=np.float32)
 			seed = np.random.randint(len(self.sentences))	
 			one_sentence = self.sentences[seed]
-			print('Starting char:')
+			print('\nStarting char:')
 			print(one_sentence)
 
 			for i, char in enumerate(one_sentence):
@@ -83,33 +81,54 @@ class Data():
 
 			return sample_matrix
 
-	def predict(self, model, num_classes, num_gen=100, use_empty_vector=False):
+	def predict(self, model, num_gen=100, use_empty_vector=False):
 		if use_empty_vector:
 			eval_matrix = self.generate_sample(zero_vectors=True)
 		else:
 			eval_matrix = self.generate_sample()
 
-		yhat_matrix = np.zeros((1, num_classes))
+		yhat_matrix = np.zeros(self.num_classes)
 		text = ''
 
 		for i in range(num_gen):
 			if use_empty_vector and i == 0:
-				yhat_matrix[0, np.random.choice(eval_matrix.shape[2])] = 1	
+				yhat_matrix[np.random.choice(eval_matrix.shape[2])] = 1	
 
 			else:
 				yhat_matrix_raw = model.predict(eval_matrix)
-				yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 1
+				yhat_matrix[np.argmax(yhat_matrix_raw)] = 1
 
 				yhat_char = self.indices_to_char(yhat_matrix)
 				text += yhat_char
 
 				eval_matrix = np.delete(eval_matrix, 0, axis=1)
-				eval_matrix = np.append(eval_matrix, [yhat_matrix], axis=1)
+				eval_matrix = np.append(eval_matrix, [[yhat_matrix]], axis=1)
 
-				yhat_matrix[0, np.argmax(yhat_matrix_raw)] = 0
+				yhat_matrix[np.argmax(yhat_matrix_raw)] = 0
 		
 		print('Prediction:')	
 		print(text)
+
+	def get_generator(self, batch_size=32):
+		while True:
+			sentence_list = []
+			target_list = []
+			for i in range(batch_size):
+				seed = np.random.choice(len(self.sentences))
+				sentence_list.append(self.sentences[seed])
+				target_list.append(self.next_char[seed])
+
+			yield self.sentences_to_matrix(batch_size, sentence_list, target_list)
+
+	def sentences_to_matrix(self, training_size, sentences, next_char):
+		x = np.zeros((training_size, self.seq_len, len(self.chars)), dtype=np.float32)
+		y = np.zeros((training_size, len(self.chars)), dtype=np.float32)
+		for i, sentence in enumerate(sentences):
+			for j, char in enumerate(sentence):
+				x[i, j, self.char_indices[char]] = 1
+			y[i, self.char_indices[next_char[i]]] = 1
+
+		return x, y
 
 	def char_to_indices(self, char):
 		mat = np.zeros(self.char_len, dtype=np.float32)
@@ -117,7 +136,12 @@ class Data():
 		return mat
 
 	def indices_to_char(self, mat):
-		idx = np.where(mat[0]==1)
+		'''
+		idx = tuples, i.e. (array([NUMBER]),)
+		idx[0] = a list with single number, i.e. [NUMBER]
+		idx[0][0] = a number
+		'''
+		idx = np.where(mat==1)
 		return self.indices_char[idx[0][0]]
 
 	def save_things(self, save_sentence):
@@ -135,3 +159,9 @@ class Data():
 				one_sentence = re.sub(r'\t', '`t`', one_sentence)
 				r.write(one_sentence)
 			r.close()
+
+	def get_num_classes(self):
+		return self.num_classes
+
+	def get_training_size(self):
+		return self.training_size
